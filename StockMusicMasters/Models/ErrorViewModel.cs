92eceff22af40fb22b59955d32e3ede832bbb170
Nicholas Glesmann <nicholasglesmann@gmail.com>
2020-02-06 11:13:51 -0800

    First Commit

---
+++ StockMusicMasters/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StockMusicMasters.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
