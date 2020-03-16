b8bac911590ee27d6185bfbc12605e811a7d4361
Nicholas Glesmann <nicholasglesmann@gmail.com>
2020-03-16 14:07:29 -0700

    Add to Standard role automatically on registration

--- StockMusicMasters/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ StockMusicMasters/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -78,6 +78,9 @@ namespace StockMusicMasters.Areas.Identity.Pages.Account
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
+                    //var currentUser = await _userManager.FindByNameAsync(user.UserName);
+                    var roleresult = await _userManager.AddToRoleAsync(user, "Standard");
+
                     _logger.LogInformation("User created a new account with password.");
 
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
